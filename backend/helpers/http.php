094c7bbdca7f1737d7712aab42dab707db897319
Mohamed Ayoub Karroud <97947325+MohamedAyoubKarroud@users.noreply.github.com>
2026-05-31 21:28:56 +0100

    Update http.php

--- backend/helpers/http.php
+++ backend/helpers/http.php
@@ -12,7 +12,7 @@ function applyCors(array $allowedOrigins): void
     header('Access-Control-Allow-Headers: Content-Type, Authorization');
 
     if (($_SERVER['REQUEST_METHOD'] ?? '') === 'OPTIONS') {
-        http_response_code(204);
+        http_response_code(200);
         exit;
     }
 }
